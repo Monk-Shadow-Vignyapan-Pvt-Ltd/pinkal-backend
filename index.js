3999d923eb0ea6bfa605a895defc2b7128cbc80c
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-02-17 12:06:32 +0530

    / removed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["https://console.pinkalhealth.ca", "https://pinkalhealth.ca","http://localhost:5173/"], // Allow both domains
+  origin: ["https://console.pinkalhealth.ca", "https://pinkalhealth.ca","http://localhost:5173"], // Allow both domains
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: [
     "Origin",

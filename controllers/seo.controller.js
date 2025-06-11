dee6babe110d42210f1db6ac0fe4fb2f95b3a600
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-06-11 19:11:13 +0530

    Fix SEo

--- controllers/seo.controller.js
+++ controllers/seo.controller.js
@@ -23,7 +23,7 @@ export const addSeo = async (req, res) => {
             .replace(/-+/g, '-');               // Replace multiple hyphens with a single hyphen
 
         const existingSeo = await Seo.findOne({ pageName });
-            let oldUrls = existingSeo.oldUrls || [];
+            let oldUrls = existingSeo.oldUrls ?  existingSeo.oldUrls : [];
             if (existingSeo) {
                 if (existingSeo.seoUrl && existingSeo.seoUrl !== urlFriendlySeoUrl && !oldUrls.includes(existingSeo.seoUrl)) {
                    await oldUrls.push(existingSeo.seoUrl);

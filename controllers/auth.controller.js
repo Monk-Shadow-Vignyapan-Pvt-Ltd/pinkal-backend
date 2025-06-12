966ae333a20398f4476edc8f918babc596d921ef
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-06-12 11:58:48 +0530

    Meta robots Add1

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -380,7 +380,7 @@ export const generateRedirectHTMLFiles = async () => {
   <meta name="title" content="${entry.title}" />
   <meta name="description" content="${entry.description}" />
   <link rel="canonical" href="${entry.urlPath === 'home' ? 'https://pinkalhealth.com' : `https://pinkalhealth.com/${entry.urlPath}`}" />
-  <meta name="robots" content="index, follow" />
+  <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
 
   <!-- Open Graph Tags -->
   <meta property="og:title" content="${entry.title}" />
